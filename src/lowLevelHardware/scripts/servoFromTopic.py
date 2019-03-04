6485b16f6673c06de92fc6d25d593aade298c891
acenturyandabit <steeven.liu2@gmail.com>
2019-03-04 14:52:01 +1100

    with statement

--- src/lowLevelHardware/scripts/servoFromTopic.py
+++ src/lowLevelHardware/scripts/servoFromTopic.py
@@ -13,22 +13,22 @@ PWM_MIN=rospy.get_param("val_min",0)
 TIME_MAX=rospy.get_param("time_max",2.00)# in ms
 TIME_MIN=rospy.get_param("time_min",1.00)
 topic=rospy.get_param("topic","thrust")
-pwm=navio.pwm.PWM(PWM_OUTPUT)
-pwm.set_period(50)
-pwm.enable()
+with navio.pwm.PWM(PWM_OUTPUT) as pwm:
+    pwm.set_period(50)
+    pwm.enable()
 
-def callback(data):
-    scaled=data.data;
-    # clip the range
-    if (scaled>PWM_MAX):
-        scaled=PWM_MAX
-    if (scaled<PWM_MIN):
-       scaled=PWM_MIN
-    # get the corresponding value
-    scaled=(scaled-PWM_MIN)/(PWM_MAX-PWM_MIN)*(TIME_MAX-TIME_MIN)+TIME_MIN;
-    pwm.set_duty_cycle(scaled)
-    # set the servo power
+    def callback(data):
+        scaled=data.data;
+        # clip the range
+        if (scaled>PWM_MAX):
+            scaled=PWM_MAX
+        if (scaled<PWM_MIN):
+        scaled=PWM_MIN
+        # get the corresponding value
+        scaled=(scaled-PWM_MIN)/(PWM_MAX-PWM_MIN)*(TIME_MAX-TIME_MIN)+TIME_MIN;
+        pwm.set_duty_cycle(scaled)
+        # set the servo power
 
-rospy.init_node("servoNode",anonymous=True)
-rospy.Subscriber(topic,Int32,callback)
-rospy.spin()
+    rospy.init_node("servoNode",anonymous=True)
+    rospy.Subscriber(topic,Int32,callback)
+    rospy.spin()

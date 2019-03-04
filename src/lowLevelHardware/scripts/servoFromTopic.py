31c8c484f047837a568196ca16f7327dd7bd636b
acenturyandabit <steeven.liu2@gmail.com>
2019-03-04 11:53:02 +1100

    servo code

---
+++ src/lowLevelHardware/scripts/servoFromTopic.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32
+import sys
+import time
+import navio.pwm
+import navio.util
+
+navio.util.check_apm()
+PWM_OUTPUT=rospy.get_param("srv_num",0)
+PWM_MAX=rospy.get_param("val_max",100)
+PWM_MIN=rospy.get_param("val_min",0)
+TIME_MAX=rospy.get_param("time_max",2.00)# in ms
+TIME_MIN=rospy.get_param("time_min",1.00)
+
+pwm=navio.pwm.PWM(PWM_OUTPUT)
+pwm.set_period(50)
+pwm.enable()
+
+def callback(data):
+    scaled=data.data;
+    # clip the range
+    if (scaled>PWM_MAX)scaled=PWM_MAX
+    if (scaled<PWM_MIN)scaled=PWM_MIN
+    # get the corresponding value
+    scaled=(scaled-PWM_MIN)/(PWM_MAX-PWM_MIN)*(TIME_MAX-TIME_MIN)+TIME_MIN;
+    pwm.set_duty_cycle(scaled)
+    # set the servo power
+	rospy.loginfo(rospy.get_caller_id()+ "%s", data.data)
+
+rospy.init_node("echobot")
+rospy.Subscriber("chatter",,callback)
+rospy.spin()
+
+
+#!/usr/bin/env python

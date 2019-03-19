f51ad5ff2f00c03e8db9662192a3bcbee9eb57d4
acenturyandabit <steeven.liu2@gmail.com>
2019-03-19 11:15:05 +1100

    ip blinker for headless init

---
+++ src/Navio2/Python/ipLED.py
@@ -0,0 +1,46 @@
+import sys
+
+import navio.leds
+import time
+import navio.util
+import socket
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
+navio.util.check_apm()
+
+led = navio.leds.Led()
+
+led.setColor('Yellow')
+print "LED is yellow"
+time.sleep(1)
+
+# fetch our IP address
+trueIP = get_ip()
+# trueIP=trueIP.split(".")
+while (True):
+    for i in trueIP:
+        try:
+            count = int(i)
+            for j in range(count):
+                led.setColor('Green')
+                time.sleep(0.2)
+                led.setColor('Black')
+                time.sleep(0.2)
+        except Exception:
+            led.setColor('Red')
+            time.sleep(0.2)
+            led.setColor('Black')
+            time.sleep(0.2)
